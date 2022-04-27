cdbb4f7f4a4114b2b8e9eb00c571af9847c8aa80
206 <206@academicos.cesde.edu.co>
2022-04-27 09:45:23 -0500

    hoy

--- js/registro.js
+++ js/registro.js
@@ -1,8 +1,11 @@
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
 
 let boton=document.getElementById("botonRegistro")
+let modalregistro=new bootstrap.Modal(document.getElementById('mensajeinformativo'))
+let textoModal = document.getElementById("mensajemodalinfo")
 
 boton.addEventListener("click",function(evento){
+    
     evento.preventDefault()
     let email=document.getElementById("email").value
     let password=document.getElementById("password").value
@@ -13,6 +16,13 @@ boton.addEventListener("click",function(evento){
         // Signed in
         const user = userCredential.user;
         console.log("exitoo")
+        textoModal.textContent="exitoo"
+        modalregistro.show()
+        let formulario=document.getElementById("formularioregistro")
+        formulario.reset()
+        setTimeout(function(){
+            modalregistro.hide()
+        },3000)
         // ...
     })
     .catch((error) => {
@@ -20,5 +30,10 @@ boton.addEventListener("click",function(evento){
         const errorMessage = error.message;
         // ..
         console.log("fallo"+errorMessage)
+        textoModal.textContent="fallamos: "+errorMessage
+        modalregistro.show()
+        setTimeout(function(){
+            modalregistro.hide()
+        },3000)
     });
 })
